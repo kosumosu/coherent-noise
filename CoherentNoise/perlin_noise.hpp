f86f0fa83533755e2be06ba4258433278aeec1e5
kosumosu <japangod@yandex.ru>
2013-05-03 19:46:30 +0400

    Achieved 1.16 sec for 6400x4800, as in master branch.

--- CoherentNoise/perlin_noise.hpp
+++ CoherentNoise/perlin_noise.hpp
@@ -22,19 +22,8 @@ namespace noise
 
 		virtual space_t evaluate(const noise_vector & point) const override
 		{
-			traverse_info_t traverse_info;
-			for (size_t i = 0; i < dimensions; i++)
-			{
-				auto node_coord = space_int_t(noise::fast_floor<space_int_t>(point[i]));
-				auto local_coord = point[i] - node_coord;
-
-				traverse_info.min_corner_coords[i] = node_coord;
-				traverse_info.local_coords[i] = local_coord;
-				traverse_info.s_curve[i] = s_curve(local_coord);
-			}
-
-			auto sampled_value = evaluator<0>::evaluate_imp(m_perturbations, m_gradients, table_size_t(0), traverse_info);
-
+			perlin_evaluator evaluator(m_perturbations, m_gradients, point);
+			auto sampled_value = evaluator.evaluate();
 			return sampled_value;
 		}
 
@@ -96,51 +85,88 @@ namespace noise
 			space_int_t node_coord;
 		};
 
-		template <size_t DIMENSION>
-		class evaluator
+		template <size_t level>
+		struct tag { };
+
+		class perlin_evaluator
 		{
 		public:
-			enum { dimension = DIMENSION };
-			template <typename ... TLevels>
-			static space_t evaluate_imp(const table_size_t perturbations[table_size], const noise_vector gradients[table_size], table_size_t perturbation,
-				const traverse_info_t & traverse_info, const TLevels & ... level_infos)
+
+			perlin_evaluator(const table_size_t perturbations[table_size], const noise_vector gradients[table_size], const noise_vector & point)
+				: m_perturbations(perturbations)
+				, m_gradients(gradients)
 			{
-				level_info_t left_level_info = { traverse_info.local_coords[dimension], traverse_info.min_corner_coords[dimension] };
-				level_info_t right_level_info = { traverse_info.local_coords[dimension] - space_t(1), traverse_info.min_corner_coords[dimension] + space_int_t(1) };
+				init_dimension(tag<0>(), point);
+			}
 
-				auto left_perturbation = perturbations[(perturbation + left_level_info.node_coord) & table_mask];
-				auto right_perturbation = perturbations[(perturbation + right_level_info.node_coord) & table_mask];
-
-				auto left_value = evaluator<dimension + 1>::evaluate_imp(perturbations, gradients, left_perturbation, traverse_info, level_infos ..., left_level_info);
-				auto right_value = evaluator<dimension + 1>::evaluate_imp(perturbations, gradients, right_perturbation, traverse_info, level_infos ..., right_level_info);
-
-				auto interpolated_value = lerp(left_value, right_value, traverse_info.s_curve[dimension]);
-				return interpolated_value;
+			space_t evaluate() const
+			{
+				return evaluate_imp(tag<0>(), table_size_t(0));
 			}
 
 		private:
+			const table_size_t * m_perturbations;
+			const noise_vector * m_gradients;
+
+			grid_vector m_min_corner_coords;
+			noise_vector m_local_coords;
+			space_t m_s_curve[dimensions];
+
+			template <size_t DIMENSION>
+			void init_dimension(tag<DIMENSION>, const noise_vector & point)
+			{
+				init_dimension_imp<DIMENSION>(point);
+				init_dimension(tag<DIMENSION + 1>(), point);
+			}
+
+			void init_dimension(tag<dimensions - 1>, const noise_vector & point)
+			{
+				init_dimension_imp<dimensions - 1>(point);
+			}
+
+			template <size_t DIMENSION>
+			void init_dimension_imp(const noise_vector & point)
+			{
+				auto node_coord = space_int_t(noise::fast_floor<space_int_t>(point[DIMENSION]));
+				auto local_coord = point[DIMENSION] - node_coord;
+
+				m_min_corner_coords[DIMENSION] = node_coord;
+				m_local_coords[DIMENSION] = local_coord;
+				m_s_curve[DIMENSION] = s_curve(local_coord);
+			}
+
+			template <size_t DIMENSION, typename ... TLevels>
+			space_t evaluate_imp(tag<DIMENSION>, table_size_t perturbation, const TLevels & ... level_infos) const
+			{
+				level_info_t left_level_info = { m_local_coords[DIMENSION], m_min_corner_coords[DIMENSION] };
+				level_info_t right_level_info = { m_local_coords[DIMENSION] - space_t(1), m_min_corner_coords[DIMENSION] + space_int_t(1) };
+
+				auto left_perturbation = m_perturbations[(perturbation + left_level_info.node_coord) & table_mask];
+				auto right_perturbation = m_perturbations[(perturbation + right_level_info.node_coord) & table_mask];
+
+				auto left_value = evaluate_imp(tag<DIMENSION + 1>(), left_perturbation, level_infos ..., left_level_info);
+				auto right_value = evaluate_imp(tag<DIMENSION + 1>(), right_perturbation, level_infos ..., right_level_info);
+
+				auto interpolated_value = lerp(left_value, right_value, m_s_curve[DIMENSION]);
+				return interpolated_value;
+			}
+
 			static space_t lerp(const space_t & left_value, const space_t & right_value, const space_t & fraction)
 			{
 				return left_value + fraction * (right_value - left_value);
 			}
-		};
 
-		template <>
-		class evaluator<dimensions>
-		{
-		public:
-			enum { dimension = 0 };
 			template <typename ... TLevels>
-			static space_t evaluate_imp(const table_size_t perturbations[table_size], const noise_vector gradients[table_size], table_size_t perturbation,
-				const traverse_info_t & traverse_info, const TLevels & ... level_infos)
+			space_t evaluate_imp(tag<dimensions>, table_size_t perturbation, const TLevels & ... level_infos) const
 			{
 				const noise_vector local_position(level_infos.local_coord ...);
 
-				auto gradient = gradients[perturbation];
+				auto gradient = m_gradients[perturbation];
 
 				auto dot_product = noise::dot(local_position, gradient);
 				return dot_product;
 			}
 		};
+
 	};
 }
